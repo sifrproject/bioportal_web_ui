2146e6dde6400757534f2a241ab2c135eab51c4c
Vincent Emonet <vincent.emonet@gmail.com>
2015-09-25 16:55:51 +0200

    fix error when deleting mapping from the UI : ontology_id and concept_id were not defined

--- app/assets/javascripts/bp_mappings.js
+++ app/assets/javascripts/bp_mappings.js
@@ -55,6 +55,9 @@ jQuery(document).bind("tree_changed", updateMappingCount);
 function deleteMappings() {
   var mappingsToDelete = [], params;
 
+  var ontology_id = jQuery(document).data().bp.ont_viewer.ontology_id;
+  var concept_id = jQuery(document).data().bp.ont_viewer.concept_id;
+
   jQuery("#delete_mappings_error").html("");
   jQuery("#delete_mappings_spinner").show();
 
