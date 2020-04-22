0eec9a76d3d1093e5cbb910cb7a19251da5ecf39
Jérôme Lamarque <jerome.lamarque@lirmm.fr>
2020-04-22 16:14:31 +0200

    Upgrading ConText component to FastContext within NCBO annotatorplus UI (cf. annotators issue #28 : https://github.com/sifrproject/annotators/issues/28). Fixing FastContext checkbox label within annotator UI.

--- app/controllers/ncbo_annotatorplus_controller.rb
+++ app/controllers/ncbo_annotatorplus_controller.rb
@@ -44,9 +44,7 @@ class NcboAnnotatorplusController < ApplicationController
                 :exclude_numbers => params[:exclude_numbers] ||= "false",  # service default is false
                 :whole_word_only => params[:whole_word_only] ||= "true", # service default is true
                 :exclude_synonyms => params[:exclude_synonyms] ||= "false",  # service default is false
-                :negation => params[:negation] ||= "false",  # service default is false
-                :experiencer => params[:experiencer] ||= "false",  # service default is false
-                :temporality => params[:temporality] ||= "false",  # service default is false
+                :fast_context => params[:fast_context] ||= "false",  # service default is false
                 :score => params[:score],
                 :score_threshold => params[:score_threshold] ||=-1,
                 :confidence_threshold => params[:confidence_threshold] ||=-1,
@@ -63,9 +61,7 @@ class NcboAnnotatorplusController < ApplicationController
     query += "&score=" + options[:score] unless options[:score] == ""
     query += "&score_threshold=" + options[:score_threshold] unless options[:score] == "" or options[:score_threshold]==-1
     query += "&confidence_threshold=" + options[:confidence_threshold] unless options[:score] == "" or options[:confidence_threshold]==-1
-    query += "&negation=" + options[:negation] unless options[:negation].empty?
-    query += "&experiencer=" + options[:experiencer] unless options[:experiencer].empty?
-    query += "&temporality=" + options[:temporality] unless options[:temporality].empty?
+    query += "&fast_context=" + options[:fast_context] unless options[:fast_context].empty?
     query += "&ontologies=" + CGI.escape(options[:ontologies].join(',')) unless options[:ontologies].empty?
     query += "&semantic_types=" + options[:semantic_types].join(',') unless options[:semantic_types].empty?
     query += "&semantic_groups=" + options[:semantic_groups].join(',') unless options[:semantic_groups].empty?
